a0018ccea276ed016eccbe904cdb54c2e300bc16
Sean Grossman <sean.grossman@gmail.com>
2015-05-13 21:41:39 -0400

    Fix frightening mutation error in flatten_hierarchy.
    The list acc was mutating object member lists instead of making a
    copy. Added a list comprehension to get a clean copy.

--- app/helpers.py
+++ app/helpers.py
@@ -16,7 +16,7 @@ def flatten_hierarchy(employee, f):
     """Walks the managements hierarchy from top to bottom and returns
     a flat list of the values returned by calling f on an Employee
     instance."""
-    acc = f(employee)
+    acc = [item for item in f(employee)]
     if employee.direct_reports == []:
         return acc
     for report in employee.direct_reports:
